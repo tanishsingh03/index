9e70a4b0f4611278f38f2d9ec02ef5eccb7f21b8
TANISH SINGH <144161401+tanishsingh03@users.noreply.github.com>
2024-01-23 11:00:32 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+//     int num = 35;
+//     char st= '*';
+//     float pi =3.14;
+//    printf("helloo\n tanish");
+//     return 0;
+
+
+// int x,y,z;
+// x=y=z=5;
+
+// printf("%d",x*y*z);
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+printf("%d",a*b*c);
+return 0;
+
+    
+}
